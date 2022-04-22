06e26c3426ff2ebe96ba81121f1fb8fe8d6c7a69
Brian Samuels <samuels.brian9@gmail.com>
2022-04-21 20:17:18 -0400

    Updating js files

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
